8bd247044f8a4175b59cc736af38a5a5d56c423c
HereBeAndre <andrea.tota6@gmail.com>
2024-02-21 14:29:28 +0100

    Add semordnilap solution

--- problems/semordnilap/solution.js
+++ problems/semordnilap/solution.js
@@ -1,6 +1,29 @@
+const reverse = (string) => {
+  let reversed = '';
+
+  for (let j = string.length - 1; j >= 0; j--) {
+    reversed += string[j];
+  }
+
+  return reversed;
+};
+
 function semordnilap(words) {
-  // Write your code here.
-  return [];
+  const wordsSet = new Set(words);
+  const pairs = [];
+
+  for (let i = 0; i < words.length; i++) {
+    const word = words[i];
+    const reversedWord = reverse(word);
+
+    if (wordsSet.has(reversedWord) && reversedWord !== word) {
+      pairs.push([word, reversedWord]);
+      wordsSet.delete(word);
+      wordsSet.delete(reversedWord);
+    }
+  }
+
+  return pairs;
 }
 
 // Do not edit the line below.
